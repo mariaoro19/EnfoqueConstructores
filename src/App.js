b2d9701dee58aaa5ab7520409be4b506ca638073
vendix2023 <print.vendix@gmail.com>
2025-02-03 20:34:46 -0500

    Arreglo librerias3

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ScrollToTop from "./pages/ScrollToTop";
 
 const CURRENT_VERSION = '1.1.5'; 
 
+
 function App() {
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
